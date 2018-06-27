952fc04cce2883f712e01d6bd71e948fe3e8d15e
yfei1 <yfei816@gmail.com>
2018-06-27 08:56:30 -0700

    Initial commit for the youtube searcher project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyC1gVVjUYvSF8vWsdxZPAT_kjiibCPQiYQ';
+
+const App = () => {
+  return <div>Hi!</div>;
+};
+
+ReactDOM.render(App(), document.querySelector('.container'));
